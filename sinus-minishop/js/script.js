ddfb42e9c5bad17d912211e143d0850a77f9593f
RahimOnGit <semo@Semos-MacBook-Pro.local>
2024-11-16 14:57:04 +0100

    done ... ready to submit

--- sinus-minishop/js/script.js
+++ sinus-minishop/js/script.js
@@ -46,7 +46,7 @@ title.innerText = "Sinus";
 
 //8
 let allP = document.querySelectorAll("p");
-for(p of allP)
+for(let p of allP)
 {
     p.style.color = "grey"
 }
@@ -61,7 +61,8 @@ for(button of allButtons)
 //10
 nav_elements[0].classList.add("active");
 //11
-document.querySelector(".logo").classList.remove("logo");
+
+//document.querySelector(".logo").classList.remove("logo");
 
 
 //12
@@ -69,6 +70,95 @@ document.querySelector(".logo").classList.remove("logo");
 let about = document.createElement("a");
 about.innerText = "about";
 nav_elements[2].appendChild(about);
-console.log(nav)
+console.log(nav_elements)
 
+
+
+
+// const product  = {
+
+// newImage : function(src="")
+// {
+// let img = document.createElement("img");
+// img.setAttribute("src",src);
+// }
+
+// ,newH2 :function()
+// {
+//     let h2 = document.createElement("h2");
+// h2.innerText = "Sinus Hoodie"
+
+// }
+
+
+// }
+
+
+
+//13
+let img = document.createElement("img");
+img.setAttribute("src","img/hoodie-forrest.png");
+
+let h2 = document.createElement("h2");
+h2.innerText = "Sinus Hoodie"
+
+let h3 = document.createElement("h3");
+h3.innerText = "forrest"
+
+let p = document.createElement("p");
+p.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolores."
+
+let b = document.createElement("button");
+b.innerText = "add to cart";
+
+let art4 = document.createElement("article");
+art4.classList.add("art-4")
+
+
+
+let fig = document.createElement("figure");
+
+
+let main = document.querySelector("main");
+
+main.appendChild(art4);
+art4.appendChild(fig);
+
+fig.insertAdjacentElement("afterbegin",img);
+art4.insertAdjacentElement("beforeend",h2);
+art4.insertAdjacentElement("beforeend",h3);
+art4.insertAdjacentElement("beforeend",p);
+art4.insertAdjacentElement("beforeend",b);
+
+
+
+
+
+document.querySelector(".logo").addEventListener("click",function()
+{
+console.log("found you");
+
+});
+
+
+
+document.querySelector(".art-1").addEventListener("click",function()
+{
+console.log("Hi i'm article Ash");
+});
+
+document.querySelector(".art-2").addEventListener("click",function()
+{
+console.log("Hi i'm article Fire")
+});
+
+document.querySelector(".art-3").addEventListener("click",function()
+{
+console.log("Hi i'm article Water")
+});
+
+document.querySelector(".art-4").addEventListener("click",function()
+{
+console.log("Hi i'm article Forrest")
+});
 
